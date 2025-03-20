2ac0432ade4cbfd197d495c08aa0759bbd8dc268
dariomvg <contact.dario.info@gmail.com>
2025-03-20 18:39:12 -0300

    add options

---
+++ src/libs/options.ts
@@ -0,0 +1,25 @@
+import { Modules } from "@/types/types";
+
+export const formats: string[] = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "link",
+    "image",
+  ];
+
+export const modules: Modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, false] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        ["clean"],
+      ],
+    },
+};
