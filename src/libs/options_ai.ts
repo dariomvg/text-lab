3bee81f2eebeb87d530d86a65b144d66b43605c2
dariomvg <contact.dario.info@gmail.com>
2025-08-07 15:58:36 -0300

    change client of googleGenAI to options file

--- src/libs/options_ai.ts
+++ src/libs/options_ai.ts
@@ -1,1 +1,5 @@
+import { GoogleGenAI } from "@google/genai";
+
 export const IA_API_KEY = process.env.GEMINI_API_KEY ?? process.env.NEXT_PUBLIC_API_KEY;
+
+export const client = new GoogleGenAI({ apiKey: IA_API_KEY });
