bbae3987a758dc9895d2acde2c9cb35cd6aa0115
dariomvg <contact.dario.info@gmail.com>
2025-08-07 15:52:24 -0300

    change place for call GoogleGenAI, now call in CustomHook

--- src/hooks/useAI.tsx
+++ src/hooks/useAI.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { useState } from "react";
 import { ObjectAI } from "@/types/types";
-import { client } from "@/libs/options_ai";
+import { IA_API_KEY } from "@/libs/options_ai";
+import { GoogleGenAI } from "@google/genai";
 
 export const useAI = () => {
   const [responsesAI, setResponsesAI] = useState<ObjectAI[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [err, setErr] = useState(null);
+  const client = new GoogleGenAI({ apiKey: IA_API_KEY });
 
   const create = async (question: string) => {
     try {
