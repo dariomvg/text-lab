451391270b6976ffc0ec76e617759969b6a30b8f
dariomvg <contact.dario.info@gmail.com>
2025-10-20 09:27:28 -0300

    change executablePath to prod and dev

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -1,30 +1,37 @@
 import { NextResponse } from 'next/server';
-import chromium from '@sparticuz/chromium';
 import puppeteer from 'puppeteer-core';
+import chromium from '@sparticuz/chromium';
 
-export const runtime = 'nodejs';
+export const runtime = 'nodejs'; 
 
 export async function POST(req: Request) {
   try {
     const { html } = await req.json();
     const isProd = process.env.NODE_ENV === 'production';
-    const executablePath = isProd ? await chromium.executablePath() : undefined;
+
+    
+    const executablePath = isProd
+      ? await chromium.executablePath()
+      : process.platform === 'win32'
+        ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+        : process.platform === 'linux'
+          ? '/usr/bin/google-chrome'
+          : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
     const browser = await puppeteer.launch({
       args: chromium.args,
+      defaultViewport: {width: 1920, height: 1080},
       executablePath,
       headless: true,
     });
 
     const page = await browser.newPage();
+    await page.setContent(
+      `<!DOCTYPE html><html><head><meta charset="utf-8" /></head><body>${html}</body></html>`,
+      { waitUntil: 'networkidle0' }
+    );
 
-    await page.setContent(`<!DOCTYPE html><html><body>${html}</body></html>`);
-
-    const pdfBuffer = await page.pdf({
-      format: 'A4',
-      printBackground: true,
-    });
-
+    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
     await browser.close();
 
     return new Response(Buffer.from(pdfBuffer), {
@@ -35,6 +42,6 @@ export async function POST(req: Request) {
     });
   } catch (err) {
     console.error('Error generando PDF:', err);
-    return NextResponse.json({ error: 'Error generando PDF' }, { status: 500 });
+    return NextResponse.json({ error: (err as Error).message }, { status: 500 });
   }
 }
